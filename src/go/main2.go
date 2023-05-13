b0983afc5a511c8f96e2e727c66ade498db9dd4f
godog77777 <49897345+godog77777@users.noreply.github.com>
2023-05-13 15:25:25 +0000

    fix bug in function (2023-05-13T15:25:25)

--- src/go/main2.go
+++ src/go/main2.go
@@ -12,3 +12,4 @@ package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-03-17T18:00:3
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-05-01T19:59:59") }
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-05-01T04:54:39") }
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-05-09T20:24:50") }
+package main\nimport "fmt"\nfunc main() { fmt.Println("update 2023-05-13T15:25:25") }
